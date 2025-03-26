c6491d25cd822af2050c3efaf19b329ec1f3da74
vahanarsarl <vahanar.add@gmail.com>
2025-03-26 01:43:24 +0000

    commit

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// Theme import
 import 'theme.dart';
+
+// Provider imports
+import 'providers/auth_provider.dart';
+import 'providers/product_provider.dart';
+import 'providers/booking_provider.dart';
+import 'providers/user_provider.dart';
+
+// Screen imports
 import 'screens/auth/splash_screen.dart';
 import 'screens/auth/greeting_screen.dart';
 import 'screens/auth/sign_up/sign_up_step1.dart';
@@ -12,47 +23,55 @@ import 'screens/auth/sign_up/finish_sign_up_screen.dart';
 import 'screens/home/home_screen.dart';
 import 'screens/home/search_screen.dart';
 import 'screens/home/search_result_screen.dart';
-import 'screens/home/filter_screen.dart'; // Réimporter FilterScreen
+import 'screens/home/filter_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Vahanar',
-      theme: AppTheme.lightTheme, // Utilisation du thème
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/greeting': (context) => const GreetingScreen(),
-        '/sign_up/step1': (context) => const SignUpStep1Screen(),
-        '/sign_up/step2': (context) => const VerifyPhoneScreen(),
-        '/sign_up/create_password': (context) => const CreatePasswordScreen(),
-        '/sign_in': (context) => const SignInScreen(),
-        '/forgot_password': (context) => const ForgotPasswordScreen(),
-        '/forgot_password/pass_email': (context) => const EmailSentScreen(),
-        '/sign_up/finish': (context) => const FinishSignUpScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/search': (context) => const SearchScreen(),
-        '/search/result': (context) => const SearchResultScreen(
-              pickupLocation: 'Default Location',
-              pickupDate: 'Mar 22',
-              dropoffDate: 'Mar 24',
-            ),
-        '/search/filter': (context) => const FilterScreen(
-              initialSortBy: 'Featured',
-              initialVehicleType: 'SUV',
-              initialFeature: 'Automatic transmission',
-              initialSeats: '4',
-              initialDriverAge: '24',
-            ),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()..initialize()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+        ChangeNotifierProvider(create: (_) => BookingProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Vahanar',
+        theme: AppTheme.lightTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/greeting': (context) => const GreetingScreen(),
+          '/sign_up/step1': (context) => const SignUpStep1Screen(),
+          '/sign_up/step2': (context) => const VerifyPhoneScreen(),
+          '/sign_up/create_password': (context) => const CreatePasswordScreen(),
+          '/sign_in': (context) => const SignInScreen(),
+          '/forgot_password': (context) => const ForgotPasswordScreen(),
+          '/forgot_password/pass_email': (context) => const EmailSentScreen(),
+          '/sign_up/finish': (context) => const FinishSignUpScreen(),
+          '/home': (context) => const HomeScreen(),
+          '/search': (context) => const SearchScreen(),
+          '/search/result': (context) => const SearchResultScreen(
+                pickupLocation: 'Default Location',
+                pickupDate: 'Mar 22',
+                dropoffDate: 'Mar 24',
+              ),
+          '/search/filter': (context) => const FilterScreen(
+                initialSortBy: 'Featured',
+                initialVehicleType: 'SUV',
+                initialFeature: 'Automatic transmission',
+                initialSeats: '4',
+                initialDriverAge: '24',
+              ),
+        },
+      ),
     );
   }
 }
